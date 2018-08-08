5299a5cb9ba176a77c255e6a855e8caf5dc60524
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-08 12:59:37 +0530

    Update comp.java

--- comp.java
+++ comp.java
@@ -3,20 +3,29 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-							String s1, s2;
-      Scanner in = new Scanner(System.in);
- 
-     
-      s1 = in.nextLine();
- 
-      
-      s2 = in.nextLine();
- 
-      if (s1.compareTo(s2) > 0)
-         System.out.println(s1);
-      else if (s1.compareTo(s2) < 0)
-         System.out.println(s2);
-      else   
-         System.out.println(s1);
-   }
+      			Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+ 		int[] a=new int[n];
+      		int i=0;
+		for(i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+				
+			}
+		}
+		System.out.println(a[n/2]);
+	}
 }
+		
+				
